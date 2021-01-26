33363a12e5545744525429b2bf3a4fe149f9952a
LeowWB <41581512+LeowWB@users.noreply.github.com>
2021-01-26 18:12:45 +0800

    skeleton and first few qns done

---
+++ L1/ex2/ex2.c
@@ -0,0 +1,97 @@
+/*************************************
+* Lab 1 Exercise 1
+* Name     : Daryl Tan
+* Matric No: A0132812R
+* Lab Group: B17
+*************************************/
+
+#include <stdio.h>
+#include <stdlib.h> //for malloc() and free()
+
+//Declaration of a Linked List Node
+
+typedef struct NODE {
+    int data;
+    struct NODE *next;
+} node;
+
+//Function Prototypes
+node *insertAt(node *, int, int, int);
+
+void printList(node *);
+
+void destroyList(node *);
+
+
+int main() {
+    node *myList = NULL;    //Empty List
+    int position, copies, newValue;
+
+    //Fill in code for input and processing
+
+    while (scanf("%i%i%i", &position, &newValue, &copies) == 3) {
+        myList = insertAt(myList, position, copies, newValue);
+    }
+
+    //Output code coded for you
+    printf("My List:\n");
+    printList(myList);
+
+    destroyList(myList);
+    myList = NULL;
+
+
+    printf("My List After Destroy:\n");
+    printList(myList);
+
+    return 0;
+}
+
+//Actual Function Implementations
+node *insertAt(node *head, int position, int copies, int newValue) {
+    //Fill in your code here
+    node *newHeadPointer = head;
+    node *prevNode = NULL;
+    node *nodeToInsertBefore = head;
+    for (int j = 0; j < position && nodeToInsertBefore != NULL; j++) {
+        prevNode = nodeToInsertBefore;
+        nodeToInsertBefore = nodeToInsertBefore->next;
+    }
+    for (int i = 0; i < copies; i++) {
+        node *newNode = (node *) malloc(sizeof(node));
+        newNode->data = newValue;
+        newNode->next = nodeToInsertBefore;
+        if (prevNode == NULL) {
+            newHeadPointer = newNode;
+        } else {
+            prevNode->next = newNode;
+        }
+        nodeToInsertBefore = newNode;
+    }
+    return newHeadPointer;
+}
+
+void printList(node *head)
+//Purpose: Print out the linked list content
+//Assumption: the list is properly null terminated
+{
+    //This function has been implemented for you
+    node *ptr = head;
+
+    while (ptr != NULL) {    //or you can write while(ptr)
+        printf("%i ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+void destroyList(node *head) {
+    //Fill in your code here
+    node *ptr = head;
+
+    while (ptr != NULL) {    //or you can write while(ptr)
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+    }
+}
---
+++ L1/ex3/ex3.c
@@ -0,0 +1,61 @@
+/*************************************
+* Lab 1 Exercise 3
+* Name     : Daryl Tan
+* Matric No: A0132812R
+* Lab Group: B17
+*************************************/
+
+#include <stdio.h>
+
+//Datatype Declarations
+typedef int (*arithFuncPtr)(int, int);
+
+
+//Function Prototypes
+int sumBetween(int x, int y);
+int power(int x, int y);
+int expel(int x, int y);
+int gcd(int x, int y);
+
+
+int main() {
+    arithFuncPtr spells[] = {sumBetween, gcd, expel, power};
+    char *spellNames[] = {"lumos", "alohomora", "expelliarmus", "sonorus"};
+
+    int spell, x, y;
+    while (scanf("%i%i%i", &spell, &x, &y) == 3) {
+        spell = spell - 1; // change to one based indexing
+        printf("%s %i\n", spellNames[spell], (*spells[spell])(x, y));
+    }
+
+    return 0;
+}
+
+int sumBetween(int x, int y) {
+    return (y - x + 1) * (y + x) / 2;
+}
+
+int gcd(int a, int b) {
+    int r, i;
+    while (b != 0) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int expel(int x, int y) {
+    while (x % y == 0) {
+        x /= y;
+    }
+    return x;
+}
+
+int power(int x, int y) {
+    int result = 1;
+    for (int i = 0; i < y; i++) {
+        result *= x;
+    }
+    return result;
+}
