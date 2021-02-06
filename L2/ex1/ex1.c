3107f0b629fe9ac7f120c7e047568c446489e82b
openorclose <interfiancee@gmail.com>
2021-02-06 15:28:40 +0800

    complete 1 2 3

--- L2/ex1/ex1.c
+++ L2/ex1/ex1.c
@@ -1,8 +1,8 @@
 /*************************************
 * Lab 2 Exercise 1
-* Name:
-* Student Id: A????????
-* Lab Group: B??
+* Name: Daryl Tan
+* Student Id: A0132821R
+* Lab Group: B17
 *************************************
 Note: Duplicate the above and fill in 
 for the 2nd member if  you are on a team
@@ -12,6 +12,7 @@ compute cluster node (Linux on x86)
 *************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>   
 #include <sys/stat.h>
 #include <unistd.h>     //for fork()
@@ -25,8 +26,24 @@ int main()
     scanf("%d", &nChild);
 
     //Spawn child processes
-
+    int childIds[nChild + 1];
+    for (int i = 1; i <= nChild; i++) {
+        int result = fork();
+        if (result == 0) {
+            sleep(rand() % 10);
+            printf("Child %d[%d]: Hello!\n", i, getpid());
+            return 0;
+        } else {
+            childIds[i] = result;
+        }
+    }
     //Wait on child processes in order
-     
+
+    for (int i = 1; i <= nChild; i++) {
+        int status;
+        waitpid(childIds[i], &status, 0);
+
+        printf("Parent: Child %d[%d] done\n", i, childIds[i]);
+    }
     return 0;
 }
--- L2/ex3/ex3.c
+++ L2/ex3/ex3.c
@@ -1,8 +1,8 @@
 /*************************************
 * Lab 2 Exercise 3
-* Name:
-* Student Id: A????????
-* Lab Group: B??
+* Name: Daryl Tan
+* Student Id: A0132821R
+* Lab Group: B17
 *************************************
 Note: Duplicate the above and fill in 
 for the 2nd member if  you are on a team
@@ -13,15 +13,18 @@ compute cluster node (Linux on x86)
 
 #include <stdio.h>
 #include <fcntl.h>      //For stat()
-#include <sys/types.h>   
+#include <sys/types.h>
 #include <sys/stat.h>
-//#include <sys/wait.h>   //for waitpid()
+#include <sys/wait.h>   //for waitpid()
 #include <unistd.h>     //for fork(), wait()
 #include <string.h>     //for string comparison etc
 #include <stdlib.h>     //for malloc()
 
+#define MAX_UNWAITED 10
+#define MAX_PATH_LENGTH 20
+#define MAX_COMMAND_LENGTH 20
 
-char** split( char* input, char* delimiter, int maxTokenNum, int* readTokenNum )
+char **split(char *input, char *delimiter, int maxTokenNum, int *readTokenNum)
 //Assumptions:
 //  - the input line is a string (i.e. with NULL character at the end)
 //  - the delimiter is a string of possible delimiters, each delimiter is single chracter
@@ -32,33 +35,33 @@ char** split( char* input, char* delimiter, int maxTokenNum, int* readTokenNum )
 //Note:
 //  - Must use the freeTokenArray to free memory after use!
 {
-    char** tokenStrArr;
-    char* tStart;   //start of token
-    int i, strSize;      
+    char **tokenStrArr;
+    char *tStart;   //start of token
+    int i, strSize;
 
     //allocate token array, each element is a char*
-    tokenStrArr = (char**) malloc(sizeof(char*) * maxTokenNum );
+    tokenStrArr = (char **) malloc(sizeof(char *) * maxTokenNum);
 
     //Nullify all entries
-    for (i = 0; i < maxTokenNum; i++){
+    for (i = 0; i < maxTokenNum; i++) {
         tokenStrArr[i] = NULL;
     }
 
     //Look for next token as separated by the delimiter
-    tStart = strtok(input, delimiter); 
+    tStart = strtok(input, delimiter);
 
     i = 0;
-    while (i < maxTokenNum && tStart){
+    while (i < maxTokenNum && tStart) {
 
         strSize = strlen(tStart);
 
         //Allocate space for token string. +1 for null terminator
-        tokenStrArr[i] = (char*) malloc(sizeof(char) * (strSize + 1) );
+        tokenStrArr[i] = (char *) malloc(sizeof(char) * (strSize + 1));
 
-        strcpy(tokenStrArr[i], tStart);    
+        strcpy(tokenStrArr[i], tStart);
 
         i++;
-        tStart = strtok(NULL, delimiter); 
+        tStart = strtok(NULL, delimiter);
     }
 
     //return number of token read
@@ -67,32 +70,34 @@ char** split( char* input, char* delimiter, int maxTokenNum, int* readTokenNum )
     return tokenStrArr;
 }
 
-void freeTokenArray(char** strArr, int size)
-{
+void freeTokenArray(char **strArr, int size) {
     int i;
 
     //Free each string first
-    for (i = 0; i < size; i++){
+    for (i = 0; i < size; i++) {
         if (strArr[i])
-            free( strArr[i] );
+            free(strArr[i]);
         strArr[i] = NULL;
     }
 
     //Free the entire array
-    free( strArr );
+    free(strArr);
 
     //Note: Caller still need to set the strArr parameter to NULL
     //      afterwards
 }
 
 
-int main()
-{
+int main() {
     char **cmdLineArgs;
-    char path[20] = ".";  //default search path
+    char path[MAX_PATH_LENGTH] = ".";  //default search path
     char userInput[121];
 
     int tokenNum;
+    int result;
+    int unwaitedIds[MAX_UNWAITED];
+    int numUnwaited = 0;
+
 
     //read user input
     printf("YWIMC > ");
@@ -102,14 +107,77 @@ int main()
 
     //split is a helper function to "tokenize" the input
     //see "stringTokenizer.c" program for more sample usage 
-    cmdLineArgs = split( userInput, " \n", 7, &tokenNum );
+    cmdLineArgs = split(userInput, " \n", 7, &tokenNum);
 
     //At this point you have the user input split neatly into token in cmdLineArg[]
 
-    while ( strcmp( cmdLineArgs[0], "quit") != 0 ){
+    while (strcmp(cmdLineArgs[0], "quit") != 0) {
 
         //Figure out which command the user want and implement below
+        if (strcmp(cmdLineArgs[0], "showpath") == 0) {
+            printf("%s\n", path);
+        } else if (strcmp(cmdLineArgs[0], "setpath") == 0) {
+            strcpy(path, cmdLineArgs[1]);
+            printf("%s\n", path);
+        } else if (strcmp(cmdLineArgs[0], "wait") == 0) {
+            int pid = atoi(cmdLineArgs[1]);
+            waitpid(pid, &result, 0);
 
+            // remove from unwaited
+            int found = 0;
+            for (int i = 0; i < numUnwaited; i++) {
+                if (unwaitedIds[i] == pid) {
+                    found = 1;
+                }
+                if (found && i < MAX_UNWAITED - 1) {
+                    // move next unwaited pids to cover up the currently waited one
+                    // but if i === last index don't need shift
+                    unwaitedIds[i] = unwaitedIds[i + 1];
+                }
+            }
+            if (!found) {
+                printf("%d not a valid child pid\n", pid);
+            }
+            numUnwaited--;
+        } else if (strcmp(cmdLineArgs[0], "pc") == 0) {
+            printf("Unwaited Child Processes:\n");
+            for (int i = 0; i < numUnwaited; i++) {
+                printf("%d\n", unwaitedIds[i]);
+            }
+        } else if (strcmp(cmdLineArgs[0], "result") == 0) {
+            printf("%d\n", result >> 8);
+        } else if (strcmp(cmdLineArgs[0], "showpath") == 0) {
+            printf("%s\n", path);
+        } else {
+            int slashLength = 1;
+            int nullLength = 1;
+            int maxTotalCommandLength = MAX_PATH_LENGTH + slashLength + MAX_COMMAND_LENGTH + nullLength;
+            char command[maxTotalCommandLength];
+            strcpy(command, path);
+            strcat(command, "/");
+            strcat(command, cmdLineArgs[0]);
+
+            struct stat fileStat;
+            if (stat(command, &fileStat)) {
+                printf("%s not found\n", command);
+            }
+
+            char *lastArg = cmdLineArgs[tokenNum - 1];
+            int isBackground = strcmp(lastArg, "&") == 0;
+            if (isBackground) {
+                // replace & with NULL
+                cmdLineArgs[tokenNum - 1] = NULL;
+            }
+            int childId = fork();
+            if (childId == 0) {
+                execv(command, cmdLineArgs);
+            } else if (isBackground) {
+                unwaitedIds[numUnwaited] = childId;
+                numUnwaited++;
+            } else {
+                waitpid(childId, &result, 0);
+            }
+        }
 
         //Prepare for next round input
 
@@ -118,9 +186,9 @@ int main()
 
         printf("YWIMC > ");
         fgets(userInput, 120, stdin);
-        cmdLineArgs = split( userInput, " \n", 7, &tokenNum );
+        cmdLineArgs = split(userInput, " \n", 7, &tokenNum);
     }
-    
+
 
     printf("Goodbye!\n");
 
