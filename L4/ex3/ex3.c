e9790fc32b8ae6b3a47b1d3de75e25a8863888fc
Daryl Tan <3646725+openorclose@users.noreply.github.com>
2021-04-15 19:38:52 +0800

    Update ex3.c

--- L4/ex3/ex3.c
+++ L4/ex3/ex3.c
@@ -1,11 +1,11 @@
 /*************************************
-* Lab 2 Exercise 1
+* Lab 4 Exercise 3
 * Name: Daryl Tan
 * Student Id: A0132821R
 * Lab Group: B17
 *************************************
 *************************************
-* Lab 2 Exercise 1
+* Lab 4 Exercise 3
 * Name: Leow Wen Bin
 * Student Id: A0184415E
 * Lab Group: B13
